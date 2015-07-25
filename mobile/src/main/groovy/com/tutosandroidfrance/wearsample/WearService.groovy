cd7f233828676da9a45fd1dd334e72aea82e5f23
ryanv78665 <superscooby7>
2015-07-25 18:23:53 -0500

    git file cleanup

--- mobile/src/main/groovy/com/tutosandroidfrance/wearsample/WearService.groovy
+++ mobile/src/main/groovy/com/tutosandroidfrance/wearsample/WearService.groovy
@@ -3,18 +3,12 @@ package com.tutosandroidfrance.wearsample
 import com.github.florent37.EmmetWearableListenerService
 import com.github.florent37.davinci.daemon.DaVinciDaemon
 import com.google.android.gms.wearable.MessageEvent
-import com.tutosandroidfrance.wearprotocol.AndroidVersion
 import com.tutosandroidfrance.wearprotocol.SmartphoneProtocol
 import com.tutosandroidfrance.wearprotocol.WearProtocol
 import groovy.json.JsonSlurper
 import groovy.transform.CompileStatic
 
-import java.util.List
 
-import retrofit.Callback
-import retrofit.RestAdapter
-import retrofit.RetrofitError
-import retrofit.client.Response
 
 @CompileStatic
 public class WearService extends EmmetWearableListenerService implements SmartphoneProtocol {
@@ -35,10 +29,7 @@ public class WearService extends EmmetWearableListenerService implements Smartph
     //when the watch sends a hello message to the  smartphone
     @Override
     public void hello() {
-        //Retrofit used to make a REST call
-        /*AndroidService androidService = new RestAdapter.Builder()
-                .setEndpoint(AndroidService.ENDPOINT)
-                .build().create(AndroidService.class)*/
+
         def slurper = new JsonSlurper()
 
         def jokesList = new ArrayList()
@@ -49,22 +40,6 @@ public class WearService extends EmmetWearableListenerService implements Smartph
 
 
         wearProtocol.onAndroidVersionsReceived(jokesList)
-        //Retrieves and deserialise the contents of my JSON file into an object List<AndroidVersion>
-
-        // let's groovify this!
-
-        /*androidService.getElements(new Callback<List<AndroidVersion>>() {
-            @Override
-            public void success(List<AndroidVersion> androidVersions, Response response) {
-
-                //envoie cette liste à la montre
-                wearProtocol.onAndroidVersionsReceived(androidVersions)
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-            }
-        })*/
 
 
     }
